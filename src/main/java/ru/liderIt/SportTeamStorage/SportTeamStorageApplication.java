bf062f07fd509da05a7ccf57e6c2f788f4575983
Stas <stas.vk88@yandex.ru>
2023-03-16 18:26:10 +0200

    Initial commit

---
+++ src/main/java/ru/liderIt/SportTeamStorage/SportTeamStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.liderIt.SportTeamStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTeamStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTeamStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/liderIt/SportTeamStorage/SportTeamStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liderIt.SportTeamStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportTeamStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
